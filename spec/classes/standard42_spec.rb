130c08ce395ca988c9671d4c3174bdab2d131a16
Alessandro Franceschi <al@lab42.it>
2013-08-30 12:35:14 +0200

    Release tagged and published to the Forge

--- spec/classes/standard42_spec.rb
+++ spec/classes/standard42_spec.rb
@@ -7,65 +7,60 @@ describe 'rhcs' do
   let(:facts) { { :ipaddress => '10.42.42.42' } }
 
   describe 'Test standard installation' do
-    it { should contain_package('rhcs').with_ensure('present') }
-    it { should contain_service('rhcs').with_ensure('running') }
-    it { should contain_service('rhcs').with_enable('true') }
+    it { should contain_package('cman').with_ensure('present') }
+    it { should contain_service('cman').with_ensure('running') }
+    it { should contain_service('cman').with_enable('true') }
     it { should contain_file('rhcs.conf').with_ensure('present') }
-  end
-
-  describe 'Test installation of a specific version' do
-    let(:params) { {:version => '1.0.42' } }
-    it { should contain_package('rhcs').with_ensure('1.0.42') }
   end
 
   describe 'Test standard installation with monitoring and firewalling' do
     let(:params) { {:monitor => true , :firewall => true, :port => '42', :protocol => 'tcp' } }
-    it { should contain_package('rhcs').with_ensure('present') }
-    it { should contain_service('rhcs').with_ensure('running') }
-    it { should contain_service('rhcs').with_enable('true') }
+    it { should contain_package('cman').with_ensure('present') }
+    it { should contain_service('cman').with_ensure('running') }
+    it { should contain_service('cman').with_enable('true') }
     it { should contain_file('rhcs.conf').with_ensure('present') }
-    it { should contain_monitor__process('rhcs_process').with_enable('true') }
+    it { should contain_monitor__process('cman').with_enable('true') }
     it { should contain_firewall('rhcs_tcp_42').with_enable('true') }
   end
 
   describe 'Test decommissioning - absent' do
     let(:params) { {:absent => true, :monitor => true , :firewall => true, :port => '42', :protocol => 'tcp'} }
-    it 'should remove Package[rhcs]' do should contain_package('rhcs').with_ensure('absent') end
-    it 'should stop Service[rhcs]' do should contain_service('rhcs').with_ensure('stopped') end
-    it 'should not enable at boot Service[rhcs]' do should contain_service('rhcs').with_enable('false') end
+    it 'should remove Package[rhcs]' do should contain_package('cman').with_ensure('absent') end
+    it 'should stop Service[rhcs]' do should contain_service('cman').with_ensure('stopped') end
+    it 'should not enable at boot Service[rhcs]' do should contain_service('cman').with_enable('false') end
     it 'should remove rhcs configuration file' do should contain_file('rhcs.conf').with_ensure('absent') end
-    it { should contain_monitor__process('rhcs_process').with_enable('false') }
+    it { should contain_monitor__process('cman').with_enable('false') }
     it { should contain_firewall('rhcs_tcp_42').with_enable('false') }
   end
 
   describe 'Test decommissioning - disable' do
     let(:params) { {:disable => true, :monitor => true , :firewall => true, :port => '42', :protocol => 'tcp'} }
-    it { should contain_package('rhcs').with_ensure('present') }
-    it 'should stop Service[rhcs]' do should contain_service('rhcs').with_ensure('stopped') end
-    it 'should not enable at boot Service[rhcs]' do should contain_service('rhcs').with_enable('false') end
+    it { should contain_package('cman').with_ensure('present') }
+    it 'should stop Service[rhcs]' do should contain_service('cman').with_ensure('stopped') end
+    it 'should not enable at boot Service[rhcs]' do should contain_service('cman').with_enable('false') end
     it { should contain_file('rhcs.conf').with_ensure('present') }
-    it { should contain_monitor__process('rhcs_process').with_enable('false') }
+    it { should contain_monitor__process('cman').with_enable('false') }
     it { should contain_firewall('rhcs_tcp_42').with_enable('false') }
   end
 
   describe 'Test decommissioning - disableboot' do
     let(:params) { {:disableboot => true, :monitor => true , :firewall => true, :port => '42', :protocol => 'tcp'} }
-    it { should contain_package('rhcs').with_ensure('present') }
-    it { should_not contain_service('rhcs').with_ensure('present') }
-    it { should_not contain_service('rhcs').with_ensure('absent') }
-    it 'should not enable at boot Service[rhcs]' do should contain_service('rhcs').with_enable('false') end
+    it { should contain_package('cman').with_ensure('present') }
+    it { should_not contain_service('cman').with_ensure('present') }
+    it { should_not contain_service('cman').with_ensure('absent') }
+    it 'should not enable at boot Service[rhcs]' do should contain_service('cman').with_enable('false') end
     it { should contain_file('rhcs.conf').with_ensure('present') }
-    it { should contain_monitor__process('rhcs_process').with_enable('false') }
+    it { should contain_monitor__process('cman').with_enable('false') }
     it { should contain_firewall('rhcs_tcp_42').with_enable('true') }
   end
 
   describe 'Test noops mode' do
     let(:params) { {:noops => true, :monitor => true , :firewall => true, :port => '42', :protocol => 'tcp'} }
-    it { should contain_package('rhcs').with_noop('true') }
-    it { should contain_service('rhcs').with_noop('true') }
+    it { should contain_package('cman').with_noop('true') }
+    it { should contain_service('cman').with_noop('true') }
     it { should contain_file('rhcs.conf').with_noop('true') }
-    it { should contain_monitor__process('rhcs_process').with_noop('true') }
-    it { should contain_monitor__process('rhcs_process').with_noop('true') }
+    it { should contain_monitor__process('cman').with_noop('true') }
+    it { should contain_monitor__process('cman').with_noop('true') }
     it { should contain_monitor__port('rhcs_tcp_42').with_noop('true') }
     it { should contain_firewall('rhcs_tcp_42').with_noop('true') }
   end
@@ -114,7 +109,7 @@ describe 'rhcs' do
 
   describe 'Test Monitoring Tools Integration' do
     let(:params) { {:monitor => true, :monitor_tool => "puppi" } }
-    it { should contain_monitor__process('rhcs_process').with_tool('puppi') }
+    it { should contain_monitor__process('cman').with_tool('puppi') }
   end
 
   describe 'Test Firewall Tools Integration' do
@@ -124,7 +119,7 @@ describe 'rhcs' do
 
   describe 'Test OldGen Module Set Integration' do
     let(:params) { {:monitor => "yes" , :monitor_tool => "puppi" , :firewall => "yes" , :firewall_tool => "iptables" , :puppi => "yes" , :port => "42" , :protocol => 'tcp' } }
-    it { should contain_monitor__process('rhcs_process').with_tool('puppi') }
+    it { should contain_monitor__process('cman').with_tool('puppi') }
     it { should contain_firewall('rhcs_tcp_42').with_tool('iptables') }
     it { should contain_puppi__ze('rhcs').with_ensure('present') }
   end
@@ -132,25 +127,25 @@ describe 'rhcs' do
   describe 'Test params lookup' do
     let(:facts) { { :monitor => true , :ipaddress => '10.42.42.42' } }
     let(:params) { { :port => '42' } }
-    it 'should honour top scope global vars' do should contain_monitor__process('rhcs_process').with_enable('true') end
+    it 'should honour top scope global vars' do should contain_monitor__process('cman').with_enable('true') end
   end
 
   describe 'Test params lookup' do
     let(:facts) { { :rhcs_monitor => true , :ipaddress => '10.42.42.42' } }
     let(:params) { { :port => '42' } }
-    it 'should honour module specific vars' do should contain_monitor__process('rhcs_process').with_enable('true') end
+    it 'should honour module specific vars' do should contain_monitor__process('cman').with_enable('true') end
   end
 
   describe 'Test params lookup' do
     let(:facts) { { :monitor => false , :rhcs_monitor => true , :ipaddress => '10.42.42.42' } }
     let(:params) { { :port => '42' } }
-    it 'should honour top scope module specific over global vars' do should contain_monitor__process('rhcs_process').with_enable('true') end
+    it 'should honour top scope module specific over global vars' do should contain_monitor__process('cman').with_enable('true') end
   end
 
   describe 'Test params lookup' do
     let(:facts) { { :monitor => false , :ipaddress => '10.42.42.42' } }
     let(:params) { { :monitor => true , :firewall => true, :port => '42' } }
-    it 'should honour passed params over global vars' do should contain_monitor__process('rhcs_process').with_enable('true') end
+    it 'should honour passed params over global vars' do should contain_monitor__process('cman').with_enable('true') end
   end
 
 end
